ce0d7f5e4f7c66e7e257dfbba8345a9259adb3c7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-15 09:55:01 +1300

    grab UI elements from html using querySelector

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ let min = 1,
   max = 10,
   winningNum = 2,
   guessesLeft = 3;
+
+const gameUI = document.querySelector("game"),
+  minNumUI = document.querySelector(".min-num"),
+  maxNumUI = document.querySelector("max-num"),
+  guessBtnUI = document.querySelector("#guess-btn"),
+  guessInputUI = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
